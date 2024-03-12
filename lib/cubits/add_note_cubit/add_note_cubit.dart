80f9320c70ae31a71e00ae89e63f601dfe2f3970
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2024-03-12 16:28:15 +0200

    add colors to bottom sheet

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -8,8 +8,9 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  bool isLoading = false;
+  Color? color = const Color(0xffAC3931);
   addNote(NoteModel note) async {
+    note.color = color!.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
