8b733dd6b91ae68ce11609981a91995f7f584ec0
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2024-03-12 17:24:58 +0200

    edit notes colors list

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/models/note_model.dart';
-import 'package:notes_app/widgets/colors_list_view.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
+import 'package:notes_app/widgets/edit_notes_colors_list.dart';
+
+import '../cubits/notes_cubit/notes_cubit.dart';
 
 class EditNoteViewBody extends StatefulWidget {
   const EditNoteViewBody({super.key, required this.note});
@@ -26,7 +29,7 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
               widget.note.title = title ?? widget.note.title;
               widget.note.subTitle = subTitle ?? widget.note.subTitle;
               widget.note.save();
-              // BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+              BlocProvider.of<NotesCubit>(context).fetchAllNotes();
               Navigator.pop(context);
             },
             title: 'Edit Note',
@@ -46,7 +49,9 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
             hint: widget.note.subTitle,
           ),
           const SizedBox(height: 16),
-          const ColorsListView(),
+          EditNotesColorsList(
+            note: widget.note,
+          ),
         ],
       ),
     );
